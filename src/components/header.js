6599fd4871b7aed0fa60e177f10235f1b97c5f0c
Sol Breslin <solbreslin@pm.me>
2020-04-25 12:52:14 +1200

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,22 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
-import styled from "styled-components";
-
-const HeaderEl = styled.header`
-  left: 0;
-  position: fixed;
-  top: 0;
-  z-index: 1;
-`;
-
-const Menu = styled.ul`
-  display: flex;
-  flex-direction: column;
-`;
-
-const MenuItem = styled.li`
-  display: flex;
-`;
 
 const Header = ({ siteTitle, menuLinks }) => (
-  <HeaderEl>
-    <Link to="/">{siteTitle}</Link>
-    <Menu>
-      {menuLinks.map(link => (
-        <MenuItem key={link.name}>
-          <Link to={link.link}>{link.name}</Link>
-        </MenuItem>
-      ))}
-    </Menu>
-  </HeaderEl>
+  <header>
+    <nav>
+      <Link to="/">{siteTitle}</Link>
+      <ul>
+        {menuLinks.map(link => (
+          <li key={link.name}>
+            <Link to={link.link} activeClassName="active">
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </header>
 );
 
 Header.propTypes = {

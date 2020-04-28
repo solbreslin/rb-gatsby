17402c5b419f5852301ae60a1023b80ac2bc489b
Sol Breslin <solbreslin@pm.me>
2020-04-28 13:47:40 +1200

    Refactor Header

--- src/components/header.js
+++ src/components/header.js
@@ -2,22 +2,58 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Header = ({ siteTitle, menuLinks }) => (
-  <header>
-    <nav>
-      <Link to="/">{siteTitle}</Link>
-      <ul>
-        {menuLinks.map(link => (
-          <li key={link.name}>
-            <Link to={link.link} activeClassName="active">
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.headerEl = React.createRef();
+    this.state = {
+      height: 0,
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState(
+      {
+        height: this.calcHeaderHeight(),
+      },
+      this.setCSSCustomProp
+    );
+  };
+
+  calcHeaderHeight = () => {
+    return this.headerEl.clientHeight;
+  };
+
+  setCSSCustomProp = () => {
+    const { height } = this.state;
+
+    const root = document.documentElement;
+    root.style.setProperty("--header-height", `${height}px`);
+  };
+
+  render = () => {
+    const { siteTitle, menuLinks } = this.props;
+
+    return (
+      <header ref={element => (this.headerEl = element)}>
+        <nav>
+          <ul>
+            {menuLinks.map(link => (
+              <li key={link.name}>
+                <Link to={link.link} activeClassName="active">
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Link to="/" className="brand">
+          {siteTitle}
+        </Link>
+      </header>
+    );
+  };
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,73 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import "./../styles/global.scss";
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          menuLinks {
-            name
-            link
-          }
-        }
-      }
-    }
-  `);
+const debounce = fn => {
+  let frame;
 
-  return (
-    <>
-      <Header
-        menuLinks={data.site.siteMetadata.menuLinks}
-        siteTitle={data.site.siteMetadata.title}
-      />
-      <main>{children}</main>
-    </>
-  );
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
 };
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.storeScroll = debounce(this.storeScroll.bind(this));
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("scroll", this.storeScroll);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("scroll", this.storeScroll);
+  };
+
+  storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+  };
+
+  render = () => {
+    const { children } = this.props;
+
+    return (
+      <>
+        <StaticQuery
+          query={graphql`
+            query {
+              site {
+                siteMetadata {
+                  title
+                  menuLinks {
+                    name
+                    link
+                  }
+                }
+              }
+            }
+          `}
+          render={data => (
+            <Header
+              menuLinks={data.site.siteMetadata.menuLinks}
+              siteTitle={data.site.siteMetadata.title}
+            />
+          )}
+        />
+        <main>{children}</main>
+      </>
+    );
+  };
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,

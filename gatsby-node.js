6b1da8532d4fbfe10504f759823213c665f60cf7
Sol Breslin <solbreslin@pm.me>
2020-08-07 18:45:35 +1200

    Add webpack rule for paper js

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,3 +44,11 @@ exports.createPages = ({ actions }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: { rules: [{ test: /node_modules\/paper/, use: loaders.null() }] },
+    });
+  }
+};
